013febc3b05edbe84c798514a67d86979dae2d6a
古閑健太郎 <kogakentarou@mba.local>
2018-01-22 15:51:58 +0900

    stickynote app with react

---
+++ stickynote-app/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Board } from './Board';//Boardコンポーネントの読み込み
+
+class App extends Component {
+  render() {
+    return (
+      <Board />
+    );
+  }
+}
+
+export default App;
